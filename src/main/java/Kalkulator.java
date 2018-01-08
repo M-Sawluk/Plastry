702579f88a0894d48f222202edc4afd4be35836d
michal.sawluk <michal.sawluk@finanteq.com>
2018-01-08 11:18:34 +0100

    5. Uzytkownik wprowadza vat

--- src/main/java/Kalkulator.java
+++ src/main/java/Kalkulator.java
@@ -6,11 +6,13 @@ public class Kalkulator {
 	private static long QUANTITY;
 	private static BigDecimal PRICE;
 	private static BigDecimal TOTAL_VALUE;
+	private static BigDecimal TAX;
 	public static void main(String[] args) {
 		scanQuantity();
 		scannPrice();
 		TOTAL_VALUE = calculateValue(QUANTITY, PRICE);
-		System.out.println(String.format("Calkowita watosc wynosci %s", TOTAL_VALUE.toString()));
+		System.out.println(String.format("Calkowita wartosc wynosci %s", TOTAL_VALUE.toString()));
+		scannTax();
 
 	}
 
@@ -26,6 +28,11 @@ public class Kalkulator {
 
 	public static BigDecimal calculateValue(long quantity, BigDecimal PRICE) {
 		return BigDecimal.valueOf(quantity).multiply(PRICE);
+	}
+
+	public static void scannTax(){
+		System.out.println("Wprowadz wartosc podatku");
+		TAX = scanner.nextBigDecimal();
 	}
 
 	public static long getQUANTITY() {
